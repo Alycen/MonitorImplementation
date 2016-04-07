28677756f31fab18270f37f25acb1caf43409ff6
alycen <alsunnid@gmail.com>
2016-04-07 09:26:53 +0100

    Initial commit

--- MonitorImplementation/MonitorImplementation.cpp
+++ MonitorImplementation/MonitorImplementation.cpp
@@ -2,10 +2,103 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <mutex>
+#include "Player.h"
 
+using namespace std;
+
+struct BoundedBuffer {
+	string* buffer;
+	int capacity;
+
+	int front;
+	int rear;
+	int count;
+
+	std::mutex lock;
+
+	std::condition_variable not_full;
+	std::condition_variable not_empty;
+
+	BoundedBuffer(int capacity) : capacity(capacity), front(0), rear(0), count(0) {
+		buffer = new string[capacity];
+	}
+
+	~BoundedBuffer() {
+		delete[] buffer;
+	}
+
+	void deposit(string data) {
+		std::unique_lock<std::mutex> l(lock);
+
+		not_full.wait(l, [this]() {return count != capacity; });
+
+		buffer[rear] = data;
+		rear = (rear + 1) % capacity;
+		++count;
+
+		not_empty.notify_one();
+	}
+
+	string fetch() {
+		std::unique_lock<std::mutex> l(lock);
+
+		not_empty.wait(l, [this]() {return count != 0; });
+
+		string result = buffer[front];
+		front = (front + 1) % capacity;
+		--count;
+
+		not_full.notify_one();
+
+		return result;
+	}
+};
+
+void consumer(int id, BoundedBuffer& buffer) {
+	for (int i = 0; i < 50; ++i) {
+		string value = buffer.fetch();
+		//cout << "Consumer " << id << " fetched " <<  value << endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(250));
+	}
+}
 
 int main()
 {
-    return 0;
+	sf::RenderWindow window(sf::VideoMode(1400, 900, 32), "This Subject makes me want to quit college");
+
+	Player* player = new Player();
+
+	BoundedBuffer buffer(200);
+
+	std::thread c1(consumer, 0, std::ref(buffer));
+	std::thread c2(consumer, 1, std::ref(buffer));
+	std::thread c3(consumer, 2, std::ref(buffer));
+
+	c1.join();
+	c2.join();
+	c3.join();
+
+	while (window.isOpen())
+	{
+		sf::Event Event;
+		while (window.pollEvent(Event))
+		{
+			InputManager::GetInstance()->UpdatePolledEvents(Event);
+			if (Event.type == sf::Event::Closed)
+				window.close();
+
+			if ((Event.type == sf::Event::KeyPressed) && (Event.key.code == sf::Keyboard::Escape))
+				window.close();
+		}
+		player->Update();
+
+		window.clear();
+		player->Draw(window);
+		window.display();
+	}
+
+	return 0;
 }
 
---
+++ MonitorImplementation/Player.h
@@ -0,0 +1,80 @@
+#ifndef _PLAYER_
+#define _PLAYER_
+
+#include "SFML\Graphics.hpp"
+#include "InputManager.h"
+
+class Player
+{
+private:
+	sf::Texture texture;
+	sf::Sprite sprite;
+
+	sf::Vector2f position;
+	sf::Vector2f direction;
+	float radius;
+public:
+	Player::Player()
+	{
+		position = sf::Vector2f(500, 500);
+
+		texture.loadFromFile("player.png");
+		sprite.setTexture(texture);
+	}
+	void Player::Update()
+	{
+		//X-Axis
+		if (InputManager::GetInstance()->IsKeyDown(sf::Keyboard::A))
+		{
+			direction.x--;
+		}
+		else if (InputManager::GetInstance()->IsKeyReleased(sf::Keyboard::A))
+		{
+			direction.x++;
+		}
+		if (InputManager::GetInstance()->IsKeyDown(sf::Keyboard::D))
+		{
+			direction.x++;
+		}
+		else if (InputManager::GetInstance()->IsKeyReleased(sf::Keyboard::D))
+		{
+			direction.x--;
+		}
+
+		//Y-Axis
+		if (InputManager::GetInstance()->IsKeyDown(sf::Keyboard::W))
+		{
+			direction.y--;
+		}
+		else if (InputManager::GetInstance()->IsKeyReleased(sf::Keyboard::W))
+		{
+			direction.y++;
+		}
+		if (InputManager::GetInstance()->IsKeyDown(sf::Keyboard::S))
+		{
+			direction.y++;
+		}
+		else if (InputManager::GetInstance()->IsKeyReleased(sf::Keyboard::S))
+		{
+			direction.y--;
+		}
+		float length = sqrt((direction.x * direction.x) + (direction.y * direction.y));
+
+		if (length > 0)
+		{
+			sf::Vector2f normalised = direction / length;
+			position += normalised * 2.0f;
+			sprite.setRotation(atan2(normalised.y, normalised.x) * 180 / (22.0f / 7.0f) + 90.0f);
+
+			sprite.setPosition(position);
+			
+		}
+
+	}
+	void Player::Draw(sf::RenderWindow &win)
+	{
+		win.draw(sprite);
+	}
+};
+
+#endif
